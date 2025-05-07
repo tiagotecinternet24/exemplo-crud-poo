70046012bba2f33b52eb14b29651e5c60329bc9d
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-05-07 09:01:57 -0300

    Create Fabricante.php

---
+++ src/Models/Fabricante.php
@@ -0,0 +1,42 @@
+<?php
+namespace ExemploCrud;
+
+use InvalidArgumentException;
+
+final class Fabricante {
+    private ?int $id;
+    private string $nome;
+
+    public function __construct(string $nome, ?int $id = null)
+    {
+        $this->setNome($nome);
+        $this->setId($id);
+        $this->validar();
+    }
+
+    private function validar():void 
+    {
+        if(empty($this->nome)){
+            throw new InvalidArgumentException("Nome é obrigatório");
+        }
+    }
+
+    public function getId(): ?int 
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    private function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+}
