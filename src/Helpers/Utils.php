89bf3048d01a5d7cc82ccaabb4505a4253981125
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-05-13 10:16:44 -0300

    Update Utils.php

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -4,7 +4,6 @@ namespace ExemploCrud\Helpers;
 use Throwable;
 
 final class Utils {
-    /* Usando construtor privado e vazio bloqueamos a criação de objeto/instância */
     private function __construct() { }
 
     public static function dump(mixed $dados):void {
